3d01bba6c49e194b16b336807f0f15cbf64a4e74
dengxiaogui <dengxg@southgnss.com>
2018-10-31 17:25:44 +0800

    提交代码

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+
+  </div>
+</template>
+
+<script>
+// import $ from 'jquery'  // 模块化开发
+// $(function () {
+//   alert(123)
+// })
+export default {
+  name: 'Header',
+  data () {
+    return {     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
